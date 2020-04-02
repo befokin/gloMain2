e828d29e16fcef1a326fedaacd23cfa7b9b04675
Boris Fokin <Befokin@mail.ru>
2020-04-02 20:04:21 +0300

    создал trainigs_03

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(5);
+console.log(!!'js');
+console.log(String(10));
+console.log(typeof String(10));
+console.log(String(true));
+
+if (true) console.log('Истина');
+if (false) console.log('Этот код не выполнится');
